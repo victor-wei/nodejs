8cf449bd2b4dc4c3a2036dc832c237230a338144
weihe <852089299@qq.com>
2016-08-23 21:53:39 -0700

    a

--- hopbcu/routes/location.js
+++ hopbcu/routes/location.js
@@ -8,9 +8,9 @@ var mongoose = require('mongoose');
 var Location = mongoose.model('Location');
 
 router.post('/hopbcu/location',function(req,res,next){
-	var jsoninfo = eval('(' + req.body.json + ')');
+	//var jsoninfo = eval('(' + req.body.json + ')');
 
-	console.log(jsoninfo);
+	//console.log(jsoninfo);
 
 	var post = '';
 
@@ -20,7 +20,7 @@ router.post('/hopbcu/location',function(req,res,next){
 	});
 	req.on('end',function(){
 
-		//post = querystring.parse(post);
+		post = querystring.parse(post);
 
 		var location = new Location(post);
 
