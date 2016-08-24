81e06ca1a2d93b71872008d663b75ee2fea357ff
weihe <852089299@qq.com>
2016-08-23 23:16:16 -0700

    a

--- hopbcu/routes/location.js
+++ hopbcu/routes/location.js
@@ -12,30 +12,16 @@ router.post('/hopbcu/location',function(req,res,next){
 
 	console.log(jsoninfo+" jsoninfo");
 
-	var post = '';
-
-	req.on('data',function(chunk){
-		post += chunk;
-		console.log("on!"+post);
+	Location.save(jsoninfo,function(err){
+		if(err){
+			res.end('Error');
+			return next();
+		}
+		res.end();
 	});
-	req.on('end',function(){
 
-		post = querystring.parse(post);
-		post = JSON.stringify(post);
-		var location = new Location(post);
-
-		console.log("OK!"+post);
-
-		location.save(function(err){
-			if(err){
-				res.end('Error');
-				return next();
-			}
-		});
-
-		res.end(util.inspect(post));
-	});
-	//res.end();
+	
+	
 });
 
 module.exports = router;
