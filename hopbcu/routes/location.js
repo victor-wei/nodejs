480249bc5dafde85effba22b0fb783347efa328d
weihe <852089299@qq.com>
2016-08-23 23:31:29 -0700

    a

--- hopbcu/routes/location.js
+++ hopbcu/routes/location.js
@@ -8,7 +8,7 @@ var mongoose = require('mongoose');
 var Location = mongoose.model('Location');
 
 router.post('/hopbcu/location',function(req,res,next){
-	var jsoninfo =  eval('(' + req.body.data + ')'); ;
+	
     var location = new Location({
     	
 		latitude:req.body.data.latitude,
