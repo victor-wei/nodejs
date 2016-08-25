5f35aa35b5687a12aed19af5e296afc3ca976e14
weihe <852089299@qq.com>
2016-08-25 05:59:09 -0700

    a

--- hopbcu/routes/index.js
+++ hopbcu/routes/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Location = mongoose.model('Location');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// router.get('/findtudou',function(req,res,next){
+// 	res.render('mymap',{title:'Findtudou',latitude:103.864527777778,longitude: 30.78425})
+// });
+
 router.get('/findtudou',function(req,res,next){
-	res.render('mymap',{title:'Findtudou',latitude:103.864527777778,longitude: 30.78425})
+
+	Location..find({year:0},function(err,docs){
+		res.render('mymap', {
+ 							items: docs
+						  });
+	}).sort({_id:-1}).limit(10);
 });
+
 
 module.exports = router;
