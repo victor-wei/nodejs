ec8201a6dd3abd47faf6c792c29e3f7d16d179a1
weihe <852089299@qq.com>
2016-08-28 08:28:23 -0700

    ss

--- hopbcu/routes/location.js
+++ hopbcu/routes/location.js
@@ -10,14 +10,19 @@ var Location = mongoose.model('Location');
 router.post('/hopbcu/location',function(req,res,next){
 	
 
-	// var send = function(){
-	// 	var positionUrl = 
-	// }
-    var location = new Location({
+    function callback(data)
+    {
+    	if(data.status !=0){
+    		return;
+    	}
+    	var points = data.result;
+    	var transresult = points[0];
+    	var point = new BMap.Point(transresult.x,transresult.y);
+    	var location = new Location({
 
-		latDegree:req.body.latDegree,
+		latDegree:transresult.Y,
 		latMin:req.body.latMin,
-		lonDegree:req.body.lonDegree,
+		lonDegree:transresult.X,
 		lonMin:req.body.lonMin,
 		year:req.body.year,
 		month:req.body.month,
@@ -26,16 +31,49 @@ router.post('/hopbcu/location',function(req,res,next){
 		minute:req.body.minute,
 		second:req.body.second,
 		dataNumber:req.body.datanumber
-    });
-	
 
-	location.save(function(err){
+		location.save(function(err){
 		if(err){
 			res.end('Error');
 			//return next();
 		}
 		res.end("save success ");
 	});
+    });
+    }
+	var send = function(){
+		var positionUrl = "http://api.map.baidu.com/geoconv/v1/?coords="+
+		(req.body.latDegree+req.body.latMin/1000/60)+','+(req.body.lonDegree+req.body.lonMin/1000/60)
+		+&from=1&to=5&ak=yTdOFtxX6c7mOnPNcCKCNn9XYrKz7XGQ&callback="+callback;
+        var script = document.createElement('script');
+        script.src = positionUrl;
+        document.getElementsByTagName("head")[0].appendChild(script);
+	}
+
+	send();
+ //    var location = new Location({
+
+	// 	latDegree:req.body.latDegree,
+	// 	latMin:req.body.latMin,
+	// 	lonDegree:req.body.lonDegree,
+	// 	lonMin:req.body.lonMin,
+	// 	year:req.body.year,
+	// 	month:req.body.month,
+	// 	day:req.body.day,
+	// 	hour:req.body.hour,
+	// 	minute:req.body.minute,
+	// 	second:req.body.second,
+	// 	dataNumber:req.body.datanumber
+ //    });
+	
+
+	// location.save(function(err){
+	// 	if(err){
+	// 		res.end('Error');
+	// 		//return next();
+	// 	}
+	// 	res.end("save success ");
+	// });
 
 	
 	
