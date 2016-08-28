d17ce717ba59dcc7c53270818cbdd6e21ebb57c0
weihe <852089299@qq.com>
2016-08-28 09:25:21 -0700

    ss

--- hopbcu/routes/location.js
+++ hopbcu/routes/location.js
@@ -9,75 +9,29 @@ var Location = mongoose.model('Location');
 
 router.post('/hopbcu/location',function(req,res,next){
 	
+    var location = new Location({
 
-    function callback(data)
-    {
-    	if(data.status !=0){
-    		return;
-    	}
-    	// var points = data.result;
-    	// var transresult = points[0];
-    	//var point = new BMap.Point(transresult.x,transresult.y);
-  //   	var location = new Location({
-
-		// latDegree:transresult.y,
-		// latMin:req.body.latMin,
-		// lonDegree:transresult.x,
-		// lonMin:req.body.lonMin,
-		// year:req.body.year,
-		// month:req.body.month,
-		// day:req.body.day,
-		// hour:req.body.hour,
-		// minute:req.body.minute,
-		// second:req.body.second,
-		// dataNumber:req.body.datanumber
-		// });
-
-		// location.save(function(err){
-		// if(err){
-		// 	res.end('Error');
-		// 	//return next();
-		// }
-		// res.end("save success ");
-	
-  //   });
-    }
-
-    var lat = req.body.latDegree+req.body.latMin/1000/60;
-    var lon = req.body.lonDegree+req.body.lonMin/1000/60;
-	var send = function(){
-		var positionUrl = "http://api.map.baidu.com/geoconv/v1/?coords=0,0"
-		//lon+','+lat
-		+"&from=1&to=5&ak=yTdOFtxX6c7mOnPNcCKCNn9XYrKz7XGQ";
-        var script = document.createElement('script');
-        script.src = positionUrl;
-        document.getElementsByTagName("head")[0].appendChild(script);
-	}
-
-	send();
- //    var location = new Location({
-
-	// 	latDegree:req.body.latDegree,
-	// 	latMin:req.body.latMin,
-	// 	lonDegree:req.body.lonDegree,
-	// 	lonMin:req.body.lonMin,
-	// 	year:req.body.year,
-	// 	month:req.body.month,
-	// 	day:req.body.day,
-	// 	hour:req.body.hour,
-	// 	minute:req.body.minute,
-	// 	second:req.body.second,
-	// 	dataNumber:req.body.datanumber
- //    });
+		latDegree:req.body.latDegree,
+		latMin:req.body.latMin,
+		lonDegree:req.body.lonDegree,
+		lonMin:req.body.lonMin,
+		year:req.body.year,
+		month:req.body.month,
+		day:req.body.day,
+		hour:req.body.hour,
+		minute:req.body.minute,
+		second:req.body.second,
+		dataNumber:req.body.datanumber
+    });
 	
 
-	// location.save(function(err){
-	// 	if(err){
-	// 		res.end('Error');
-	// 		//return next();
-	// 	}
-	// 	res.end("save success ");
-	// });
+	location.save(function(err){
+		if(err){
+			res.end('Error');
+			//return next();
+		}
+		res.end("save success ");
+	});
 
 	
 	
